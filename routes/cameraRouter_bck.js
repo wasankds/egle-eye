998a3c09ac24dd4de1180a93eb9587e5d225f65f
wasankds <wasankds@gmail.com>
2025-11-16 12:04:19 +0700

    Sun 11/16/2025 12:04:19.56

--- routes/cameraRouter_bck.js
+++ routes/cameraRouter_bck.js
@@ -119,15 +119,15 @@ router.post(PATH_REQUEST,  async (req, res) => {
 
     //=== ใช้ - servo2
     if(direction == 'left'){ // ใช้ - servo2      
-      let sub = 7
+      let sub = 10
       LEFT_START += sub
-      if(LEFT_START > 120) LEFT_START = 120
+      if(LEFT_START > 180) LEFT_START = 180
       myServo.setAngle(global.servo2, LEFT_START, 600, 2400)
       return  res.send({ status: 'ok left', direction: direction });
     }else if(direction == 'right'){ // ใช้ - servo2
-      let sub = 7
+      let sub = 10
       RIGHT_START -= sub
-      if(RIGHT_START < 60) RIGHT_START = 60
+      if(RIGHT_START < 0) RIGHT_START = 0
       myServo.setAngle(global.servo2, RIGHT_START, 600, 2400)
       return  res.send({ status: 'ok right', direction: direction });
     }
@@ -135,13 +135,13 @@ router.post(PATH_REQUEST,  async (req, res) => {
     else if(direction == 'up'){ // ใช้ - servo1
       let sub = 10
       UP_START += sub
-      if(UP_START > 120) UP_START = 120
+      if(UP_START > 180) UP_START = 180
       myServo.setAngle(global.servo1, UP_START, 600, 2400)
       return  res.send({ status: 'ok up', direction: direction });  
     }else if(direction == 'down'){ // ใช้ - servo1
       let sub = 10
       DOWN_START -= sub
-      if(DOWN_START < 60) DOWN_START = 60
+      if(DOWN_START < 0) DOWN_START = 0
       myServo.setAngle(global.servo1, DOWN_START, 600, 2400)
       return  res.send({ status: 'ok down', direction: direction });
     }
