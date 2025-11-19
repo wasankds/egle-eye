1179c78b92f1ceb426a7fc44eff6e74a3a4eb9d7
wasankds <wasankds@gmail.com>
2025-11-19 15:08:41 +0700

    Wed 11/19/2025 15:08:41.37

--- start-stream.sh
+++ start-stream.sh
@@ -7,5 +7,6 @@ fi
 
 # Start RTSP stream (run forever)
 # Workaround: encode with libx264 to ensure RTSP compatibility
-rpicam-vid --width 640 --height 480 --codec h264 --framerate 15 --inline --timeout 0 -o - | \
+rpicam-vid --width 720 --height 480 --codec h264 --framerate 15 --inline --timeout 0 -o - | \
 ffmpeg -re -fflags +genpts -r 15 -i - -c:v libx264 -preset ultrafast -tune zerolatency -f rtsp rtsp://localhost:8554/stream
+ 
