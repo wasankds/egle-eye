8d8eeed31624d1ef6dc5a81cb759dd4ab85a97ee
wasankds <wasankds@gmail.com>
2025-11-21 09:47:12 +0700

    Fri 11/21/2025  9:47:12.61

--- scripts/record-and-extract.sh
+++ scripts/record-and-extract.sh
@@ -17,7 +17,7 @@ tee \
       -c:v copy \
       -f segment -segment_time $SEGMENT_TIME -reset_timestamps 1 -strftime 1 "$VIDEO_DIR/%Y-%m-%d_%H-%M-%S.mp4" ) \
     | ffmpeg -y -f h264 -analyzeduration 10000000 -probesize 10000000 -i - \
-      -vf "fps=7,scale=320:180" -strftime 1 "$EXTRACT_DIR/%Y-%m-%d_%H-%M-%S_%03d.jpg"
+      -vf "fps=10,scale=720:480" -strftime 1 "$EXTRACT_DIR/%Y-%m-%d_%H-%M-%S_%03d.jpg"
 
 # 2. ลบไฟล์เก่าอัตโนมัติ (mp4/jpg)
 while true; do
